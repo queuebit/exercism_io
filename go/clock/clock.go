ba8ed20a3a0bc26462f977f3e1012a5578690d27
queuebit <queuebit10648@gmail.com>
2015-03-12 12:39:04 -0400

    First iteration that passes clock tests

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -11,16 +11,28 @@ type Clock int
 func Time(hour, minute int) Clock {
   var min = hour * 60 + minute
   c := Clock(min)
+  for (c < 0) {
+    c = c + 1440
+  }
+  if (c >= 1440) {
+    c = c - 1440
+  }
   return c
 }
 
 func (c Clock) Add(minutes int) Clock {
   c = c + Clock(minutes)
+  for (c < 0) {
+    c = c + 1440
+  }
+  if (c >= 1440) {
+    c = c - 1440
+  }
   return c
 }
 
 func (c Clock) String() string {
-  return fmt.Sprintf("%d:%d", c/60, c%60)
+  return fmt.Sprintf("%02d:%02d", c/60, c%60)
 }
 
 func (c Clock) Equal(d Clock) bool {
